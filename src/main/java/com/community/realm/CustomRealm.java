8f7d3e4cf04c434b865905ec84727cec9f8a5680
Jackson-Yuan <1436169116@qq.com>
2020-02-18 14:29:27 +0800

    用户验证

--- src/main/java/com/community/realm/CustomRealm.java
+++ src/main/java/com/community/realm/CustomRealm.java
@@ -1,16 +1,31 @@
 package com.community.realm;
 
+import com.community.dao.UserMapper;
+import com.community.entity.User;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by yyc on 2020/2/17.
  */
+/**
+* @Description: shiro中的Realm组件，进行授权，验证操作
+* @Param:
+* @Return:
+* @Author: yyc
+*/
 public class CustomRealm extends AuthorizingRealm{
+
+    @Autowired
+    private UserMapper userMapper;
+
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
         return null;
@@ -18,6 +33,12 @@ public class CustomRealm extends AuthorizingRealm{
 
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
-        return null;
+        String userName = (String)authenticationToken.getPrincipal();
+
+        User user = userMapper.selectByPrimaryKey(userName);
+
+        ByteSource salt = ByteSource.Util.bytes(userName);
+
+        return new SimpleAuthenticationInfo(user.getUserId(), user.getPassword(), salt, getName());
     }
 }
